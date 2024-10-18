3ec72a02003cf9c2781982f5af796d0da1fbed20
kagacewho <whykpsad@tutamail.com>
2024-10-18 20:14:25 +0300

    1

--- fibonacci.py
+++
@@ -1,7 +0,0 @@
-fib1 = fib2 = 1
-n = int(input('Введите число: '))
-print('Числа Фебуначи: ')
-print(0, fib1, fib2, end='')
-for i in range(2, n):
-    fib1, fib2 = fib2, fib1 + fib2
-    print(fib2, end ='')
