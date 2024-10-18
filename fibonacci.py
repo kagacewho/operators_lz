ba0476a5d004a24b880d0e5ceca987afd20766cc
kagacewho <whykpsad@tutamail.com>
2024-10-18 15:45:29 +0300

    fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -1,18 +1,7 @@
-print('Введите число:')
-n = (int(input()))
-fib = ['0']
-per1 = 0
-per2 = 1 
-for i in range (2, n + 2):
-    if i % 2 == 1: 
-        per1 = per1 + per2
-        fib.append(str(per1))
-    else: 
-        per2 = per1 + per2
-        fib.append(str(per2))
-ans = '' 
-for i in range(len(fib)):
-    ans += fib[i] + ", " 
-ans = ans[:-2] 
-print('Вот ряд Фибоначи из', n, 'не равных нулю чисел:')
-print(ans)
+fib1 = fib2 = 1
+n = int(input('Введите число: '))
+print('Числа Фебуначи: ')
+print(0, fib1, fib2, end='')
+for i in range(2, n):
+    fib1, fib2 = fib2, fib1 + fib2
+    print(fib2, end ='')
